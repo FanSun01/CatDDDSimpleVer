2a048133cb3bff7e9e48ed739b0f7c0562a148e3
FrankieSun <frankie_sun95@foxmail.com>
2022-08-20 03:46:05 +0800

    3636

--- CatSimpleVer.Common/DB/BaseDBConfig.cs
+++ CatSimpleVer.Common/DB/BaseDBConfig.cs
@@ -8,6 +8,55 @@ namespace CatSimpleVer.Common.DB
 {
     public class BaseDBConfig
     {
+        //字段 声明主从数据库配置列表,简单组合避免创建新类
+        public static (List<MutiDbConfig> allDbs, List<MutiDbConfig> slaveDbs) MutiDbConnString;
 
+        public (List<MutiDbConfig>, List<MutiDbConfig>) MutiInitConn()
+        {
+            List<MutiDbConfig> listdatabaseSimpleDB = new List<MutiDbConfig>();//单库
+            List<MutiDbConfig> listdatabaseSlaveDB = new List<MutiDbConfig>();//从库
+
+
+
+
+
+            return (listdatabaseSimpleDB, listdatabaseSlaveDB);
+        }
+    }
+
+
+    public enum DataBaseType
+    {
+        MySql = 0,
+        SqlServer = 1,
+        Sqlite = 2,
+        Oracle = 3,
+        PostgreSQL = 4,
+        Dm = 5,
+        Kdbndp = 6,
+    }
+
+    public class MutiDbConfig
+    {
+        /// <summary>
+        /// 连接启用开关
+        /// </summary>
+        public bool Enabled { get; set; }
+        /// <summary>
+        /// 连接ID
+        /// </summary>
+        public string ConnId { get; set; }
+        /// <summary>
+        /// 从库执行级别，越大越先执行
+        /// </summary>
+        public int HitRate { get; set; }
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        public string Connection { get; set; }
+        /// <summary>
+        /// 数据库类型
+        /// </summary>
+        public DataBaseType DbType { get; set; }
     }
 }
--- CatSimpleVer.Extensions/ServiceSetup/SqlsugarSetup.cs
+++ CatSimpleVer.Extensions/ServiceSetup/SqlsugarSetup.cs
@@ -23,6 +23,17 @@ namespace CatSimpleVer.Extensions.ServiceSetup
             //config存在静态属性
             MainDb.CurrentDbConnId = Appsettings.app("MainDB");
 
+            //SqlSugarScope用单例AddSingleton 单例; SqlSugarClient用 AddScoped 每次请求一个实例 ; 2选1只能用一种方式
+            //SqlSugarScope是线程安全的，这边用单例注入
+            services.AddSingleton<ISqlSugarClient>(o =>
+            {
+                var configList = new List<ConnectionConfig>();
+                var configList_Slave = new List<SlaveConnectionConfig>();
+
+
+
+                return new SqlSugarScope(configList);
+            });
 
         }
     }
