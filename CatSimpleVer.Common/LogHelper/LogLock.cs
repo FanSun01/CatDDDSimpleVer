00b243c712b73802256d09806cf5ca4768da5d3e
FrankieSun <frankie_sun95@foxmail.com>
2022-09-04 00:50:50 +0800

    f

--- CatSimpleVer.Common/LogHelper/LogLock.cs
+++ CatSimpleVer.Common/LogHelper/LogLock.cs
@@ -40,6 +40,7 @@ namespace CatSimpleVer.Common.LogHelper
         {
             try
             {
+                _ReaderWriterLockSlim.EnterWriteLock();
 
 
 
@@ -51,12 +52,45 @@ namespace CatSimpleVer.Common.LogHelper
             }
             finally
             {
-
+                //退出写入模式，释放资源占用
+                //注意：一次请求对应一次释放
+                //      若释放次数大于请求次数将会触发异常[写入锁定未经保持即被释放]
+                //      若请求处理完成后未释放将会触发异常[此模式不下允许以递归方式获取写入锁定]
+                _ReaderWriterLockSlim.ExitWriteLock();
             }
         }
 
         public static void OutSql2LogToDB(string prefix, string[] dataParas, bool IsHeader = true)
         {
+
+            string logContent = String.Join("", dataParas);
+            if (IsHeader)
+            {
+                logContent = (String.Join("", dataParas));
+            }
+            switch (prefix)
+            {
+                case "AOPLog":
+                    _log.Info(logContent);
+                    break;
+                case "AOPLogEx":
+                    _log.Error(logContent);
+                    break;
+                case "RequestIpInfoLog":
+                    _log.Debug(logContent);
+                    break;
+                case "RecordAccessLogs":
+                    _log.Debug(logContent);
+                    break;
+                case "SqlLog":
+                    _log.Info(logContent);
+                    break;
+                case "RequestResponseLog":
+                    _log.Debug(logContent);
+                    break;
+                default:
+                    break;
+            }
 
         }
 
