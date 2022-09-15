5e048f2606a8ab95ecb1755096f5417126388608
maomaoqiu <frankie.fanyang.sun@outlook.com>
2022-09-15 23:50:35 +0800

    aa

---
+++ CatSimpleVer.WebApi/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CatSimpleVer.Common.Helper;
+//using CatSimpleVer.IServices;
+using CatSimpleVer.Model;
+using CatSimpleVer.Model.Models;
+using CatSimpleVer.Model.ViewModels;
+//using CatSimpleVer.SwaggerHelper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using StackExchange.Profiling;
+
+namespace CatSimpleVer.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+    }
+}
