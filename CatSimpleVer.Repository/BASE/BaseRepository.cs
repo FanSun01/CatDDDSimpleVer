a8e82f06249e6343a6a8005df494cc85af1fe8d8
maomaoqiu <frankie.fanyang.sun@outlook.com>
2022-09-18 01:52:47 +0800

    upod

--- CatSimpleVer.Repository/BASE/BaseRepository.cs
+++ CatSimpleVer.Repository/BASE/BaseRepository.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SqlSugar;
 
 namespace CatSimpleVer.Repository.BASE
 {
     public class BaseRepository<TEntity> where TEntity : class, new()
     {
+        private readonly SqlSugarScope _db;
+
+        
+
+
 
     }
 }
--- CatSimpleVer.Repository/UOW/UnitOfWork.cs
+++ CatSimpleVer.Repository/UOW/UnitOfWork.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SqlSugar;
+using Microsoft.Extensions.Logging;
+
 
 namespace CatSimpleVer.Repository.UOW
 {
     public class UnitOfWork
     {
+        private readonly ISqlSugarClient _sqlSugarClient;
+        private readonly ILogger<UnitOfWork> _logger;
+        private int _transCount { get; set; }
+        public int TransCount => _transCount;
+
+        public UnitOfWork(ISqlSugarClient sqlSugarClient, ILogger<UnitOfWork> logger)
+        {
+            _sqlSugarClient = sqlSugarClient;
+            _logger = logger;
+            _transCount = 0;
+        }
+
+        public SqlSugarScope GetDbClient()
+        {
+            return _sqlSugarClient as SqlSugarScope;
+        }
+
+
+
+
 
     }
 }
